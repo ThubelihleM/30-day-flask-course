481399b4cd14136c598e53ec11d668db0fe0529b
Thubelihle Mdlalose <thubelihlemdlalose77@gmail.com>
2025-10-16 13:00:16 +0200

    Day 2: Added Jinja2 templates and base layout

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,27 +1,30 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)       # Create a flask app instance
 
 @app.route('/')             # Route for home page
 def home():
-  return 'Hello, Flask!'    # Return text to browser
+  return render_template('home.html')
 
 @app.route('/about')
 def about():
-  return 'This is the About Page'
+  return render_template('about.html')
 
 @app.route('/user/<name>')
 def user(name):
-  return f'Hello {name}!'
+  user_info = {'name': name, 'age': 25}
+  return render_template('user.html', user_info=user_info)
 
-@app.route('/contact')
-def contact():
-  return 'Contact Us at support@example.com'
+@app.route('/fruits')
+def fruits():
+  fruits_list = ['Apple', 'Banana', 'Orange', 'Mango']
+  return render_template('fruits.html', fruits=fruits_list)
 
-@app.route('/greet/<username>')
-def greet(username):
-  return f'Welcome, {username}!'
+@app.route('/hobbies')
+def hobbies():
+  hobbies = ['Reading', 'Gaming', 'Coding']
+  return render_template('hobbies.html', hobbies=hobbies)
 
 if __name__ ==  '__main__':
   app.run(debug=True)       # Run app in debug mode
